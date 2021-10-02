aa2bb2d159f419fdcf54160532dbc0a2c949a5ce
Женя <lapko.eugene@gmail.com>
2021-10-02 17:41:04 +0300

    add connection to db

--- web/src/main/java/com/epam/esm/configuration/SpringConfig.java
+++ web/src/main/java/com/epam/esm/configuration/SpringConfig.java
@@ -1,11 +1,32 @@
 package com.epam.esm.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.esm")
 @EnableWebMvc
 public class SpringConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/labs");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("12345");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
 }
