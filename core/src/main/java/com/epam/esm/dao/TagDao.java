3751e5d50e106d3247deb6c03bdf42d08865ca47
Женя <lapko.eugene@gmail.com>
2021-10-02 17:54:47 +0300

    update TagDao

--- core/src/main/java/com/epam/esm/dao/TagDao.java
+++ core/src/main/java/com/epam/esm/dao/TagDao.java
@@ -1,5 +1,25 @@
 package com.epam.esm.dao;
 
+import com.epam.esm.entity.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
 public class TagDao {
-    
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final String GET_ALL = "SELECT * FROM tag";
+
+
+    @Autowired
+    public TagDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Tag> getAll() {
+        return jdbcTemplate.query(GET_ALL, new BeanPropertyRowMapper<>());
+    }
 }
