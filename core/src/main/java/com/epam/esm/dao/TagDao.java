0f81694b9ce0987875d2d44a68f7efeb7e7f8d70
Женя <lapko.eugene@gmail.com>
2021-10-03 01:15:21 +0300

    update TagDao

--- core/src/main/java/com/epam/esm/dao/TagDao.java
+++ core/src/main/java/com/epam/esm/dao/TagDao.java
@@ -4,15 +4,20 @@ import com.epam.esm.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class TagDao {
 
-    private final JdbcTemplate jdbcTemplate;
-
+    private static final String GET_BY_ID = "SELECT * FROM tag WHERE id = ?";
+    private static final String CREATE = "INSERT INTO tag(name) VALUES (?)";
     private static final String GET_ALL = "SELECT * FROM tag";
+    private static final String DELETE = "DELETE FROM tag WHERE id = ?";
 
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public TagDao(JdbcTemplate jdbcTemplate) {
@@ -21,5 +26,29 @@ public class TagDao {
 
     public List<Tag> getAll() {
         return jdbcTemplate.query(GET_ALL, new BeanPropertyRowMapper<>());
+    }
+
+    public Optional<Tag> getById(Long id) {
+        return getTagForSingleResult(GET_BY_ID, id);
+    }
+
+    public void create(Tag tag) {
+        jdbcTemplate.update(CREATE, tag.getName());
+    }
+
+    public void deleteById(Long id) {
+        jdbcTemplate.update(DELETE, id);
+    }
+
+    private Optional<Tag> getTagForSingleResult(String query, Object... params) {
+        List<Tag> tags = jdbcTemplate.query(query, new BeanPropertyRowMapper<>(), params);
+
+        if (tags.size() == 1) {
+            return Optional.of(tags.get(0));
+        } else if (tags.size() > 1) {
+            throw new IllegalArgumentException("Size of list more than 1");
+        }
+
+        return Optional.empty();
     }
 }
