646ba869b536d99d21d6287e39d26fa571f14680
Женя <lapko.eugene@gmail.com>
2021-10-03 19:01:36 +0300

    create TagController

---
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -0,0 +1,30 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.entity.Tag;
+import com.epam.esm.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tag")
+public class TagController {
+
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping(value = "getAll", produces = "application/json")
+    public ResponseEntity<?> getAll() {
+        List<Tag> tags = tagService.getAll();
+        return new ResponseEntity<>(tags, HttpStatus.OK);
+    }
+}
