773bed73c30b03d515f4f3df7b0f704e26569eff
Женя <lapko.eugene@gmail.com>
2021-10-03 19:10:51 +0300

    update TagController : add method getTagById

--- web/src/main/java/com/epam/esm/controller/TagController.java
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,8 @@ import java.util.List;
 @RequestMapping("/tag")
 public class TagController {
 
+    private static final String PRODUCES = "application/json";
+
     private final TagService tagService;
 
     @Autowired
@@ -22,9 +25,17 @@ public class TagController {
         this.tagService = tagService;
     }
 
-    @GetMapping(value = "getAll", produces = "application/json")
+    @GetMapping(value = "getAll", produces = PRODUCES)
     public ResponseEntity<?> getAll() {
         List<Tag> tags = tagService.getAll();
         return new ResponseEntity<>(tags, HttpStatus.OK);
     }
+
+    @GetMapping(value = "getById", produces = PRODUCES)
+    public ResponseEntity<?> getTagById(@RequestParam("id") Long id) {
+        Tag tag = tagService.getById(id);
+        return new ResponseEntity<>(tag, HttpStatus.OK);
+    }
+
+    
 }
