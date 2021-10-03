42947a98c9486eacdf17b978e80dbba3311e3eba
Женя <lapko.eugene@gmail.com>
2021-10-03 20:27:35 +0300

    create GiftCertificateController

---
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -0,0 +1,21 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gift")
+public class GiftCertificateController {
+
+    private static final String PRODUCES = "application/json";
+
+    private final GiftCertificateService giftCertificateService;
+
+    @Autowired
+    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+        this.giftCertificateService = giftCertificateService;
+    }
+
+}
