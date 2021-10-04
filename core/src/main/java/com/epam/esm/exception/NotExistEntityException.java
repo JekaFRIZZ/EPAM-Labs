be3ab615ae293cd82b1ca2584bd4a8f72d6e0cd3
Женя <lapko.eugene@gmail.com>
2021-10-04 12:02:42 +0300

    update ResourceNotFoundException

--- core/src/main/java/com/epam/esm/exception/NotExistEntityException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.esm.exception;
-
-public class NotExistEntityException extends RuntimeException {
-
-    public NotExistEntityException(String message) {
-        super(message);
-    }
-}
